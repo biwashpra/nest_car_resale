6ac2b4c9041671e0b65d3735698f5b79cd67c10f
Biwash Pradhan <biwashpra3883@gmail.com>
2025-04-06 21:26:46 +0545

    feat: use of useGlobalPipes and feat: use of useGlobalPipes and

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // whitelist is used to strip down the object to only the properties that are defined in the DTO
+      whitelist: true,
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
