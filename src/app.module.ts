3fd277a2072108b2e9513b5500fd9d7f3d03a1fd
Biwash Pradhan <biwashpra3883@gmail.com>
2025-04-06 21:00:02 +0545

    feat: creat and connection of Reports Entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,14 @@ import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/users.entity';
+import { Reports } from './reports/reports.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'db.sqlite',
-      entities: [User],
+      entities: [User, Reports],
       synchronize: true,
     }),
     UsersModule,
